43ec05ad1da4e4458e7424a47a728451a7f809f1
Michał Kamiński (michkam455) <michkam455@student.polsl.pl>
2019-03-20 20:16:16 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Football League
+Do you want to be up to date with Your favourite team performance?
+Football League Legends is something for You!
---
+++ src/main/java/pl/polsl/take/footballleague/Main.java
@@ -0,0 +1,7 @@
+package pl.polsl.take.footballleague;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello Stowski");
+    }
+}
