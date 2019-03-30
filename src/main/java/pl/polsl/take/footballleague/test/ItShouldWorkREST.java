8e6f22e2df6cec240b803dc5a93326eb5ed23eba
Paweł Stowski <pstowski@gmail.com>
2019-03-30 14:59:29 +0100

    Configured maven to produce deployable .war file

---
+++ src/main/java/pl/polsl/take/footballleague/test/ItShouldWorkREST.java
@@ -0,0 +1,13 @@
+package pl.polsl.take.footballleague.test;
+
+
+import javax.ws.rs.*;
+@Path("/echo")
+public class ItShouldWorkREST{
+
+    @Path("/{echo}")
+    @GET
+    public String echo(@PathParam("echo") String echo) {
+        return "Halko halko "+echo+".";
+    }
+}
---
+++ src/main/java/pl/polsl/take/footballleague/test/RestApplicationConfig.java
@@ -0,0 +1,9 @@
+package pl.polsl.take.footballleague.test;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplicationConfig extends Application {
+    // intentionally empty
+}
