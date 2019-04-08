52e428b429d077f1d7b138df78a7802cd449d2c8
Paweł Stowski <pstowski@gmail.com>
2019-04-08 19:12:42 +0200

    removed test sources

--- src/main/java/pl/polsl/take/footballleague/test/RestApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package pl.polsl.take.footballleague.test;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class RestApplicationConfig extends Application {
-    // intentionally empty
-}
