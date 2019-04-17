7b61d24c21bbcdbe17cda774e2006f13d57283cd
TheRudno <therudno@gmail.com>
2019-04-17 13:05:08 +0200

    test git

---
+++ src/main/java/pl/polsl/take/footballleague/dto/GoalListDTO.java
@@ -0,0 +1,8 @@
+package pl.polsl.take.footballleague.dto;
+
+import java.util.List;
+
+public class GoalListDTO {
+
+
+}
--- src/main/java/pl/polsl/take/footballleague/rest/GoalREST.java
+++ src/main/java/pl/polsl/take/footballleague/rest/GoalREST.java
@@ -1,4 +1,33 @@
 package pl.polsl.take.footballleague.rest;
 
+
+import pl.polsl.take.footballleague.ApplicationConfig;
+import pl.polsl.take.footballleague.database.Goal;
+import pl.polsl.take.footballleague.dto.GoalListDTO;
+import pl.polsl.take.footballleague.service.GoalServiceBean;
+
+import javax.ejb.EJB;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/goal")
 public class GoalREST {
+
+    @EJB
+    GoalServiceBean goalService;
+
+
+    @GET
+    @Path("/")
+    @Produces(ApplicationConfig.DEFAULT_MEDIA_TYPE)
+    public Response getAll() {
+        List<Goal> goals = goalService.getAll();
+        return Response
+                .ok()
+                .entity(GoalListDTO.from(goals))
+                .build();
+    }
 }
